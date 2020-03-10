3ae3f0ba1da4a27594f89d0fd29b04d09ee8a53a
Atanas Chuchulev <atanas.v4@gmail.com>
2020-03-10 14:56:47 +0200

    Create main.tf

---
+++ dev/main.tf
@@ -0,0 +1,10 @@
+variable "multiple" {
+  default = 2
+  }
+
+resource "null_resource" "HelloDev" {
+  count = var.multiple
+  provisioner "local-exec" {
+    command = "echo Hello Dev ${count.index + 1}!"
+  }
+}

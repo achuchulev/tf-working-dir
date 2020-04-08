81558974c738bfbf4a0f92fced6ff61085ffe7fc
Atanas Chuchulev <atanas.v4@gmail.com>
2020-04-08 10:26:18 +0300

    Update main.tf

--- root/dev/main.tf
+++ root/dev/main.tf
@@ -1,3 +1,3 @@
 module "private_null" {
-  source = "git@github.com:achuchulev/tf-null-count-priv.git//module/tf/null?ref=v0.0.2"
+  source = "git@github.com:achuchulev/tf-null-count-priv.git//module/tf/null?ref=v0.0.3"
 }

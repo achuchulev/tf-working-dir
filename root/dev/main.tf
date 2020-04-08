5fc1c7594cdcca4a4edc649e341805dcf262b9f3
Atanas Chuchulev <atanas.v4@gmail.com>
2020-04-08 09:38:01 +0300

    Update main.tf

--- root/dev/main.tf
+++ root/dev/main.tf
@@ -2,9 +2,15 @@ variable "multiple" {
   default = 1
 }
 
-resource "null_resource" "HelloDev" {
-  count = var.multiple
-  provisioner "local-exec" {
-    command = "echo Hello Dev ${count.index + 1}!"
-  }
+#resource "null_resource" "HelloDev" {
+#  count = var.multiple
+#  provisioner "local-exec" {
+#    command = "echo Hello Dev ${count.index + 1}!"
+#  }
+#}
+
+
+
+module "private_null" {
+  source = "git@github.com:achuchulev/tf-null-count-priv.git"
 }
